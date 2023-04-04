548de6afbc69a1cb6393a876aaa89205a682a41c
Suryansh Panwar <panwarsuryansh2@gmail.com>
2023-04-04 22:28:49 +0530

    some files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const resourceGroups = [
   "Resource Group 2",
   "Resource Group 3",
 ];
-const resources = ["Resource 1", "Resource 2", "Resource 3"];
+const resources = ["Resource 1", "Resource 2", "Resource 3","Resource 4"];
 const otherOptions = ["Option 1", "Option 2", "Option 3"];
 
 function App() {
